795085e4da4c2859b1f52caa3f9a1a871d68a043
Cynthia Mory <cynthia.mory.morales@gmail.com>
2023-03-18 13:04:12 -0400

    added variables to my .js file

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ var generateBtn = document.querySelector("#generate");
 var password = document.querySelector("#password");
 
 // Critia prompts for password to generate
-var uppercase
+var uppercase = confirm("Does your password require an uppercase?");
+var lowercase = confirm("Does your password require lowercase letter?");
+var symbols = confirm("Does your password require the use of symbols?")
+var numbers = confirm("Does your password resquire the use of numbers?")
+var lengthOfPassword = prompt("Password must be minimum 8 characters and maximum 128 characters")
+
+// Password variables for allowable passwords characters
+var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
+var lowercaseABC ="abcdefghijklmnopqrstuvwxyz";
+var specialSymbols ="!@#$%^&*()?.<\>|=+:;,[-_]"
+var numeric ="0123456789"
 
 // Write password to the #password input
 function writePassword() {
